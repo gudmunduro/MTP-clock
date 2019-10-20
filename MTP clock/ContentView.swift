0734d260b94c1c2290e9497a1504be55d605d0cc
Guðmundur Óli Halldórsson <goh140400@gmail.com>
2019-10-20 17:53:07 +0000

    Initial Commit

---
+++ MTP clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MTP clock
+//
+//  Created by Guðmundur Halldórsson on 20/10/2019.
+//  Copyright © 2019 Guðmundur Halldórsson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
